1807df74c99c4f07357844276d6e2ea582f41bae
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2024-03-17 21:11:42 +0300

    Know the date

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
+
+
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
@@ -8,8 +10,11 @@ const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
 
+
+
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
+
 
 dateOptionsSelectElement.addEventListener("change", () => {
 
